a376726fa46df1e7ae7b34fe737643c9605af3a9
kzuniga88 <35325475+kzuniga88@users.noreply.github.com>
2018-02-18 17:54:44 -1000

    Update tb_hex2seg.v

--- tb_hex2seg.v
+++ tb_hex2seg.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Lianne Rozzelle, Kristel Zuniga
 // 
 // Create Date: 02/06/2018 01:38:06 PM
 // Design Name: 
