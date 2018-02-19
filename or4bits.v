d4567a19520779ce05a82a58d34de02295384ac0
Lianne Rozzelle <35238540+liannero@users.noreply.github.com>
2018-02-18 17:17:02 -1000

    Update or4bits.v

--- or4bits.v
+++ or4bits.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Lianne Rozzelle, Kristel Zuniga
 // 
 // Create Date: 02/13/2018 01:38:45 PM
 // Design Name: 
